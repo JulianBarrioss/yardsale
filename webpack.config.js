0561c31f9eab1f3bcbf12b7e7c7a1c9bb37652b7
Julian Barrios <julianbarrios0821@gmail.com>
2021-11-09 17:44:18 -0600

    React with css and sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     entry: "./src/index.js",
@@ -32,6 +33,15 @@ module.exports = {
                         loader: "html-loader"
                     }
                 ]
+            },
+
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader",
+                ],
             }
         ]
     },
@@ -41,5 +51,15 @@ module.exports = {
             template: "./public/index.html",
             filename: "./index.html"
         }),
-    ]
+
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+        }),
+    ],
+    
+    devServer: {
+        static: path.join(__dirname, "dist"),
+        compress: true,
+        port: 3005,
+    }
 }
