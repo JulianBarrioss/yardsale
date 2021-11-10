4e1ba15d6a9fa803613b325c65824d6f4afef20b
Julian Barrios <julianbarrios0821@gmail.com>
2021-11-09 18:32:59 -0600

    styles with React

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import "../styles/global.scss";
+import Layout from "../containers/Layout";
+import Login from '../containers/Login';
+import "../styles/global.css";
 
 const App = () => {
     return (
-        <h1>
-            :D, D:
-        </h1>
+       <Layout>
+           <Login />
+       </Layout>
     )
 }
 
-export default App
+export default App;
