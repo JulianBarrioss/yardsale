a3ce8aac8a92f4ea8e9da3ac83086501118f78b0
Julian Barrios <julianbarrios0821@gmail.com>
2021-11-11 18:05:47 -0600

    images and aliases with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,14 @@ module.exports = {
     
     resolve: {
         extensions: [".js", ".jsx"],
+        alias: {
+            "@components": path.resolve(__dirname, "src/components/"),
+            "@containers": path.resolve(__dirname, "src/containers/"),
+            "@pages": path.resolve(__dirname, "src/pages/"),
+            "@styles": path.resolve(__dirname, "src/styles/"),
+            "@icons": path.resolve(__dirname, "src/assets/icons/"),
+            "@logos": path.resolve(__dirname, "src/assets/logos/"),
+        }
     },
 
     module: {
@@ -43,6 +51,11 @@ module.exports = {
                     "css-loader",
                     "sass-loader",
                 ],
+            },
+
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                type: "asset"
             }
         ]
     },
