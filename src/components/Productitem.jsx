4a232d1e6e2a1c018f4778234dad55caf2d081a9
Julian Barrios <julianbarrios0821@gmail.com>
2021-11-13 15:54:33 -0600

    Custom Hooks

--- src/components/Productitem.jsx
+++ src/components/Productitem.jsx
@@ -3,9 +3,9 @@ import '@styles/ProductItem.scss';
 
 import addToCart from "@icons/bt_add_to_cart.svg";
 
-const product = "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940";
+const productImg = "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940";
 
-const ProductItem = () => {
+const ProductItem = ({ product }) => {
 	const [cart, setCart] = useState([]);
 
 	const handleClick = () => {
@@ -14,11 +14,11 @@ const ProductItem = () => {
 
 	return (
 		<div className="ProductItem">
-			<img src={product} alt="" />
+			<img src={product.images[0]} alt={product.title} />
 			<div className="product-info">
 				<div>
-					<p>$120,00</p>
-					<p>Bike</p>
+					<p>${product.price}</p>
+					<p>{product.title}</p>
 				</div>
 				<figure onClick={handleClick} >
 					<img src={addToCart} alt="" />
