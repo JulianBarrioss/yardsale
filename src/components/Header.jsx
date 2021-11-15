f23bfd06c1fd123590d140ff569c41768f86d066
Julian Barrios <julianbarrios0821@gmail.com>
2021-11-15 10:37:16 -0600

    shopping cart count

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,16 @@
-import React, { useState } from "react";
-import "@styles/Header.scss";
+import React, { useState, useContext } from 'react';
 
-import Menu from "@components/Menu";
-import menu from "@icons/icon_menu.svg";
-import logo from "@logos/logo_yard_sale.svg";
-import shoppingCart from "@icons/icon_shopping_cart.svg"
+import Menu from '@components/Menu';
+import menu from '@icons/icon_menu.svg';
+import logo from '@logos/logo_yard_sale.svg';
+import shoppingCart from '@icons/icon_shopping_cart.svg';
+import AppContext from '@context/AppContext';
+
+import '@styles/Header.scss';
 
 const Header = () => {
   const [toggle, setToggle] = useState(false);
+  const { state } = useContext(AppContext); 
 
   const handleToggle = () => {
     setToggle(!toggle);
@@ -15,40 +18,40 @@ const Header = () => {
 
   return (
     <nav>
-      <img src={menu} alt="menu" className="menu" />
-      <div className="navbar-left">
-        <img src={logo} alt="logo" className="nav-logo" />
+      <img src={menu} alt='menu' className='menu' />
+      <div className='navbar-left'>
+        <img src={logo} alt='logo' className='nav-logo' />
 
         <ul>
           <li>
-            <a href="/">All</a>
+            <a href='/'>All</a>
           </li>
           <li>
-            <a href="/">Clothes</a>
+            <a href='/'>Clothes</a>
           </li>
           <li>
-            <a href="/">Electronics</a>
+            <a href='/'>Electronics</a>
           </li>
           <li>
-            <a href="/">Furnitures</a>
+            <a href='/'>Furnitures</a>
           </li>
           <li>
-            <a href="/">Toys</a>
+            <a href='/'>Toys</a>
           </li>
           <li>
-            <a href="/">Others</a>
+            <a href='/'>Others</a>
           </li>
         </ul>
       </div>
 
-      <div className="navbar-right">
+      <div className='navbar-right'>
         <ul>
-          <li className="navbar-email" onClick={handleToggle}>
+          <li className='navbar-email' onClick={handleToggle}>
             platzi@example.com
           </li>
-          <li className="navbar-shopping-cart">
-            <img src={shoppingCart} alt="shopping cart" />
-            <div>2</div>
+          <li className='navbar-shopping-cart'>
+            <img src={shoppingCart} alt='shopping cart' />
+              {state.cart.length > 0 ? <div>{state.cart.length}</div> : null}
           </li>
         </ul>
       </div>
